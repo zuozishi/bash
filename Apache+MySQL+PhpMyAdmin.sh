009c694a1fcef048f469d552675a1f925642576e
zuozishi <wwb123123@outlook.com>
2018-09-17 22:29:36 +0800

    update

--- Apache+MySQL+PhpMyAdmin.sh
+++ Apache+MySQL+PhpMyAdmin.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-echo '正在安装MySQL...'
+echo 'Installing MySQL...'
 apt install -y mysql-server mysql-client libmysqlclient-dev
-echo '正在安装Nginx...'
+echo 'Installing Apache2...'
 apt install -y apache2 libapache2-mod-php
-echo '正在安装PHP...'
+echo 'Installing PHP...'
 apt install -y php php-mysql php-fpm php-mbstring php-gettext
-echo '正在安装PhpMyAdmin...'
+echo 'Installing PhpMyAdmin...'
 apt install -y phpmyadmin
 ln -s /usr/share/phpmyadmin /var/www/html/phpmyadmin
-echo '正在配置MySQL...'
+echo 'Installing MySQL...'
 pwsh config-mysql.ps1
---
+++ data/test.ps1
@@ -0,0 +1,1 @@
+$txt=Get-Content my.cnf
