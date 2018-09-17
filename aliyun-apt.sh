6d5f9a1fbca1f9eae5b31d0d97c448e788049f47
zuozishi <wwb123123@outlook.com>
2018-09-17 18:49:23 +0800

    update

--- aliyun-apt.sh
+++ aliyun-apt.sh
@@ -1,4 +1,5 @@
-sudo cp ./data/aliyun-source.list /etc/apt/source.list
+#!/bin/bash
+cp ./data/aliyun-source.list /etc/apt/source.list
 echo '已更换为阿里云apt源'
-sudo apt update
+apt update
 echo 'apt更新完成'
--- install-dotnet.sh
+++ install-dotnet.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 echo '正在安装Microsoft软件包Provider...'
 wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb && sudo dpkg -i packages-microsoft-prod.deb
 rm packages-microsoft-prod.deb
