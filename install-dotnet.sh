3e0ecc122576efe4e8de95901930e2669d643674
zuozishi <wwb123123@outlook.com>
2018-09-17 18:34:43 +0800

    update\

--- install-dotnet.sh
+++ install-dotnet.sh
@@ -2,9 +2,9 @@ echo '正在安装Microsoft软件包Provider...'
 wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb && sudo dpkg -i packages-microsoft-prod.deb
 rm packages-microsoft-prod.deb
 echo '正在更新apt...'
-sudo apt update
+apt update
 echo '正在安装apt https支持...'
-sudo apt-get install apt-transport-https
+apt-get install apt-transport-https
 echo '正在安装.Net Core SDK...'
-sudo apt-get install dotnet-sdk-2.1
+apt-get install dotnet-sdk-2.1
 echo '安装完成'
