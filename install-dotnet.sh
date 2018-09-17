9d089c7c6830b7343213b6ac20650440422c5400
zuozishi <wwb123123@outlook.com>
2018-09-17 18:29:05 +0800

    update

--- install-dotnet.sh
+++ install-dotnet.sh
@@ -1,5 +1,4 @@
-wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb
-sudo dpkg -i packages-microsoft-prod.deb
+wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb && sudo dpkg -i packages-microsoft-prod.deb
 rm packages-microsoft-prod.deb
 sudo apt-get install apt-transport-https
 sudo apt-get update
